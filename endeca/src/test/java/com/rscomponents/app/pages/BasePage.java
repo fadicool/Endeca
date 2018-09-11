f7158c8ae4e90eb037bde73d340d017770d111ae
Fadele Adu <fadeleadu@gmail.com>
2018-09-11 19:47:07 +0100

    changes made i.e. new classes created - in the endeca project

---
+++ endeca/src/test/java/com/rscomponents/app/pages/BasePage.java
@@ -0,0 +1,11 @@
+package com.rscomponents.app.pages;
+
+/**
+ * Created by Laptop on 9/11/2018.
+ */
+public class BasePage {
+    public static void main(String [] arg){
+        System.out.println("*** Just testing ***");
+    }
+
+}
---
+++ endeca/src/test/java/com/rscomponents/app/pages/BrandPage.java
@@ -0,0 +1,8 @@
+package com.rscomponents.app.pages;
+
+/**
+ * Created by Laptop on 9/11/2018.
+ */
+public class BrandPage extends BasePage {
+    
+}
